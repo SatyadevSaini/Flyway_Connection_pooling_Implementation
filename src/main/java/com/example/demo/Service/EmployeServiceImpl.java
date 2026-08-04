2f02e73c5788356dd1ebc6829a6151675ee3df4d
Satyadev Saini <satyadev.saini@hostbooks.com>
2026-08-05 00:42:19 +0530

    update Api

---
+++ src/main/java/com/example/demo/Service/EmployeServiceImpl.java
@@ -0,0 +1,47 @@
+package com.example.demo.Service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.Entity.Employee;
+import com.example.demo.Repo.EmployeRepo;
+
+@Service
+public class EmployeServiceImpl implements EmployeService {
+	
+	@Autowired
+	EmployeRepo repo;
+
+	@Override
+	public Employee add(Employee emp) {
+		return repo.save(emp) ;
+	}
+
+	@Override
+	public List<Employee> get() {
+		return repo.findAll();
+	}
+
+	@Override
+	public List<Employee> getnamelike() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Employee update(Integer id, Employee emp) {
+		
+		Employee existsemp = repo.findById(id).orElseThrow(() ->
+        new RuntimeException("Employee not found with id : " + id));
+		
+		existsemp.setContactNumber(emp.getContactNumber());
+		existsemp.setName(emp.getName());
+		existsemp.setSalary(emp.getSalary());
+		
+		return repo.save(existsemp);
+		
+	}
+
+}
---
+++ src/main/java/com/example/demo/controller/EmployeController.java
@@ -0,0 +1,45 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.Entity.Employee;
+import com.example.demo.Service.EmployeServiceImpl;
+
+@RestController
+@RequestMapping("/emp")
+public class EmployeController {
+	
+	@Autowired
+	EmployeServiceImpl service;
+	
+	@PostMapping("/add")
+	public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee){
+		Employee saveEmp = service.add(employee);
+		return new ResponseEntity<>(saveEmp , HttpStatus.CREATED);
+	}
+	
+	@GetMapping("/getAll")
+	public ResponseEntity<List<Employee>> getAll(){
+		List<Employee> data= service.get();
+		return new ResponseEntity<>(data , HttpStatus.OK);
+	}
+	
+	@PutMapping("/update/{id}")
+	public ResponseEntity<Employee> update(@PathVariable Integer id , @RequestBody Employee emp){
+		Employee data= service.update(id, emp);
+		return new ResponseEntity<>(data , HttpStatus.OK);
+	}
+
+}
